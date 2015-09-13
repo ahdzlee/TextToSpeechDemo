450df8cb0da966ac56cca09a9e0f12f488da7526
Ahdzlee Formentera <dev.ahdzlee@gmail.com>
2015-09-13 17:06:36 +0800

    Implemented basic text to speech demo.

--- app/src/main/java/com/ahdzlee/texttospeech/MainActivity.java
+++ app/src/main/java/com/ahdzlee/texttospeech/MainActivity.java
@@ -1,31 +1,37 @@
 package com.ahdzlee.texttospeech;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private EditText             mEtInput;
+    private FloatingActionButton mFab;
+
+    private TextToSpeech mTts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mEtInput = (EditText) findViewById(R.id.etInput);
+        setupToolbar();
+        setupFab();
+
+        mTts = new TextToSpeech(this, this);
     }
 
     @Override
@@ -48,5 +54,67 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setupFab() {
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                speak(mEtInput.getText().toString());
+            }
+        });
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = mTts.setLanguage(Locale.ENGLISH);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA ||
+                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Snackbar.make(mFab, R.string.tts_lang_not_supported, Snackbar.LENGTH_LONG).show();
+
+                mFab.setEnabled(false);
+                mEtInput.setEnabled(false);
+            } else {
+                Snackbar.make(mFab, R.string.ready_to_speak, Snackbar.LENGTH_LONG).show();
+
+                mFab.setEnabled(true);
+                mEtInput.setEnabled(true);
+            }
+        } else {
+            Snackbar.make(mFab, R.string.cannot_speak, Snackbar.LENGTH_LONG).show();
+
+            mFab.setEnabled(false);
+            mEtInput.setEnabled(false);
+        }
+    }
+
+    private void speak(String text) {
+        if (TextUtils.isEmpty(text)) {
+            mEtInput.setError(getString(R.string.error_text_empty));
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mTts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            mTts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mTts != null) {
+            mTts.stop();
+            mTts.shutdown();
+        }
+        super.onDestroy();
     }
 }
